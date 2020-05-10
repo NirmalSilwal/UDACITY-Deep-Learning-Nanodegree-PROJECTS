48fd55e5c6992d04c6bb61984a3e664ba747b4dd
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-05-10 19:44:43 +0530

    function: lambda function in AWS that triggers the deployed endpoint

---
+++ 5. Deploying a Sentiment Analysis Model/Lambda function/lambda_function.py
@@ -0,0 +1,22 @@
+# We need to use the low-level library to interact with SageMaker since the SageMaker API
+# is not available natively through Lambda.
+import boto3
+
+def lambda_handler(event, context):
+
+    # The SageMaker runtime is what allows us to invoke the endpoint that we've created.
+    runtime = boto3.Session().client('sagemaker-runtime')
+
+    # Now we use the SageMaker runtime to invoke our endpoint, sending the review we were given
+    response = runtime.invoke_endpoint(EndpointName = 'sagemaker-pytorch-2020-05-10-10-40-35-086',    # The name of the endpoint we created
+                                       ContentType = 'text/plain',                 # The data format that is expected
+                                       Body = event['body'])                       # The actual review
+
+    # The response is an HTTP response whose body contains the result of our inference
+    result = response['Body'].read().decode('utf-8')
+
+    return {
+        'statusCode' : 200,
+        'headers' : { 'Content-Type' : 'text/plain', 'Access-Control-Allow-Origin' : '*' },
+        'body' : result
+    }
